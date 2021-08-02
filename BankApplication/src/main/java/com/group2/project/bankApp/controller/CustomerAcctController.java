6a91a02aa5b168734106f16ea633f30eb67ebe15
XtC4 <80109414+XtC4@users.noreply.github.com>
2021-08-01 21:58:35 -0400

    Update CustomerAcctController.java

--- BankApplication/src/main/java/com/group2/project/bankApp/controller/CustomerAcctController.java
+++ BankApplication/src/main/java/com/group2/project/bankApp/controller/CustomerAcctController.java
@@ -1,5 +1,54 @@
 package com.group2.project.bankApp.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.group2.project.bankApp.bean.Customer;
+import com.group2.project.bankApp.bean.CustomerAcct;
+import com.group2.project.bankApp.dao.CustomerAcctDao;
+
+
+
 public class CustomerAcctController {
+	CustomerAcctDao dao;
+	
+	@RequestMapping("/accountlist")
+	public String viewBooklist(Model m, Customer c) {
+		List<CustomerAcct> list = dao.getAccounts(c);
+		System.out.print(list);
+		m.addAttribute("list", list);
+		return "accountlist";
+	}
+	
+	@RequestMapping(value = "/deposit/{id}")
+	public String deposit(@PathVariable int id, Model m) {
+		CustomerAcct account = new CustomerAcct();
+		account.setAcctNo(id);
+		m.addAttribute("command", account);
+		return "redirect:/accountlist";
+	}
+	
+	@RequestMapping(value = "/draw/{id}")
+	public String draw(@PathVariable int id, Model m) {
+		CustomerAcct account = new CustomerAcct();
+		account.setAcctNo(id);
+		m.addAttribute("command", account);
+		return "redirect:/accountlist";
+	}
+	
+	@RequestMapping(value = "/transfer/{id}")
+	public String transfer(@PathVariable int id, Model m) {
+		CustomerAcct account = new CustomerAcct();
+		account.setAcctNo(id);
+		m.addAttribute("command", account);
+		return "redirect:/accountlist";
+	}
+
 
 }
