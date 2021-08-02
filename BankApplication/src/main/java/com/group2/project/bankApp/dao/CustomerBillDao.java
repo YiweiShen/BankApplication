f2afc3e849bb5953baea933031207022dd140d33
YiWeiShen <YiWeiShen@users.noreply.github.com>
2021-08-02 19:01:55 +0800

    update customerBillDao for getBill function

--- BankApplication/src/main/java/com/group2/project/bankApp/dao/CustomerBillDao.java
+++ BankApplication/src/main/java/com/group2/project/bankApp/dao/CustomerBillDao.java
@@ -1,11 +1,39 @@
 package com.group2.project.bankApp.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.group2.project.bankApp.bean.Customer;
+import com.group2.project.bankApp.bean.CustomerBill;
+
 /**
  * @author Chun Ting Yiu, Xutong Chen, Yiwei Shen
  * @date 31 July 2021
  * @description this is a dao class for crud operations of CustomerBill Table
  **/
 
-public class CustomterBillDao {
+public class CustomerBillDao {
+	JdbcTemplate template;
+
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
+	
+	public List<CustomerBill> getBill(Customer c) {
+		return template.query("select * from customerBillTbl where customerId= " + c.getCustomerId(), new RowMapper<CustomerBill>() {
+			public CustomerBill mapRow(ResultSet rs, int row) throws SQLException {
+				CustomerBill b = new CustomerBill();
+				b.setBillerName(rs.getString(1));
+				b.setBillerAcctNo(rs.getInt(2));
+				b.setAmount(rs.getDouble(3));
+				b.setCustomerId(rs.getInt(4));		
+				return b;
+			}
+		});
+	}
 
 }
